280bc5dbcbcaa7fa344bab9185bf07bd7358c73c
akacross <jhnbenton@gmail.com>
2024-08-28 08:11:36 -0400

    Update LSPD Mod.lua
    Small changes to the if statments.

--- LSPD Mod.lua
+++ LSPD Mod.lua
@@ -10,24 +10,15 @@ function main()
         if not sampIsChatInputActive() and not sampIsDialogActive() then
             if wasKeyPressed(vkeys.VK_1) then
                 sampSendChat("/tazer")
-            
-                elseif wasKeyPressed(vkeys.VK_2) then
-                sampSendChat("/s Put your fucking hands in air where i see them, NOW ((/hu))")
-            end
-
-                elseif wasKeyPressed(vkeys.VK_3) then
-                sampSendChat("/m Park your vehicle on the right side of road and turn off the engine! ((/car engine))")
-            end
-
-                elseif wasKeyPressed(vkeys.VK_4) then
+            elseif wasKeyPressed(vkeys.VK_2) then
+                sampSendChat("/s Put your fucking hands in air where I see them, NOW ((/hu))")
+            elseif wasKeyPressed(vkeys.VK_3) then
+                sampSendChat("/m Park your vehicle on the right side of the road and turn off the engine! ((/car engine))")
+            elseif wasKeyPressed(vkeys.VK_4) then
                 sampSendChat("/wanted")
-            end
-
-                elseif wasKeyPressed(vkeys.VK_6) then
+            elseif wasKeyPressed(vkeys.VK_6) then
                 sampSendChat("/gate")
-            end
-
-                elseif wasKeyPressed(vkeys.VK_NUMPAD2) then
+            elseif wasKeyPressed(vkeys.VK_NUMPAD2) then
                 sampSendChat("/r 2-T-611, 10-41 from '19, 10-8 citywide, over")
                 sampSendChat("/d 2-T-611, On-duty, active patrol and responding to calls")
             end
